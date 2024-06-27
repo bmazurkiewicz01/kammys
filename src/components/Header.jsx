506807b0419bd6b49b1235082fee1d5e6d9e8257
bmazurkiewicz01 <bartek.mazurkiewicz69@gmail.com>
2024-06-27 19:05:23 +0200

    Improved ThemeButton component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,22 @@
 import { Link } from "react-router-dom";
-import logo from "../assets/logo.svg";
+import { useEffect, useState } from "react";
+import darkLogo from "../assets/logo-dark.svg";
+import lightLogo from "../assets/logo.svg";
 import ThemeButton from "./ThemeButton";
 import "./Header.css";
 
 const Header = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const onThemeChange = (oldTheme) => {
+    setIsDarkMode(oldTheme === "dark" ? false : true);
+  };
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme") === "dark";
+    setIsDarkMode(savedTheme);
+  }, []);
+
   const openMenu = () => {
     const menu = document.querySelector(".menu");
     const menuBackdrop = document.querySelector(".menu-backdrop");
@@ -33,14 +46,14 @@ const Header = () => {
         <div
           className="mx-auto max-w-6xl w-full relative rounded-full flex bg-white bg-opacity-40 px-4 min-h-14 backdrop-blur-xl
           items-center max-h-14 border border-[rgba(191,204,217,0.5)] 
-          shadow-[0_0_1px_rgba(85,166,246,0.1),1px_1.5px_2px_-1px_rgba(85,166,246,0.15),4px_4px_12px_-2.5px_rgba(85,166,246,0.15)]"
+          shadow-[0_0_1px_rgba(85,166,246,0.1),1px_1.5px_2px_-1px_rgba(85,166,246,0.15),4px_4px_12px_-2.5px_rgba(85,166,246,0.15)] dark:bg-black dark:bg-opacity-40 dark:border-[rgba(76,89,103,0.3)] dark:text-white"
         >
           <img
-            src={logo}
+            src={isDarkMode ? darkLogo : lightLogo}
             alt="Company truck logo"
             className="w-12 h-auto cursor-pointer"
           />
-          <nav className="hidden sm:block">
+          <nav className="hidden w-full sm:flex">
             <ul className="flex items-center justify-start gap-3 flex-grow font-sans font-normal font text-[0.90rem] pl-3">
               <li>
                 <Link to="/">Home</Link>
@@ -55,6 +68,7 @@ const Header = () => {
                 <Link to="/">Placeholder</Link>
               </li>
             </ul>
+            <ThemeButton classList={"w-full ml-auto"} onClick={onThemeChange} />
           </nav>
           <div
             className="p-3 h-full w-10 flex flex-col justify-between items-center cursor-pointer sm:hidden gap-1 ml-auto hover:bg-[rgba(85,166,246,0.3)] transition-all duration-500 ease-[cubic-bezier(0.4,0,0.2,1)] rounded-xl"
@@ -80,7 +94,7 @@ const Header = () => {
         onClick={closeMenu}
       ></div>
       <div className="menu fixed hidden z-50 top-0 right-0 h-screen w-1/2 bg-[rgb(251,252,254)] dark:bg-[rgb(18,27,33)] text-[rgb(19,17,32)] dark:text-[rgb(191,204,217)] p-4">
-        <ThemeButton />
+        <ThemeButton onClick={onThemeChange} />
         <ul className="flex flex-col items-start gap-4 mt-14 text-[rgb(19,17,32)] dark:text-[rgb(191,204,217)]">
           <li>
             <Link to="/" onClick={closeMenu}>
--- src/components/ThemeButton.jsx
+++ src/components/ThemeButton.jsx
@@ -1,10 +1,12 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useId } from "react";
 import { ReactComponent as Sun } from "../assets/Sun.svg";
 import { ReactComponent as Moon } from "../assets/Moon.svg";
+import PropTypes from "prop-types";
 import "./ThemeButton.css";
 
-const ThemeButton = () => {
+const ThemeButton = ({ classList, onClick }) => {
   const [theme, setTheme] = useState("light");
+  const id = useId();
 
   useEffect(() => {
     let savedTheme = localStorage.getItem("theme");
@@ -20,7 +22,9 @@ const ThemeButton = () => {
     if (savedTheme === "dark") {
       document.documentElement.classList.add("dark");
       document.querySelector("body").classList.add("dark");
-      document.querySelector(".dark_mode_input").checked = true;
+      document.querySelectorAll(".dark_mode_input").forEach((input) => {
+        input.checked = true;
+      });
     }
   }, []);
 
@@ -36,25 +40,35 @@ const ThemeButton = () => {
       document.documentElement.classList.remove("dark");
       document.querySelector("body").classList.remove("dark");
     }
+    onClick && onClick(theme);
   };
 
   return (
-    <div className="ml-auto w-max dark:outline-[rgba(156,204,252,0.1)] dark:border-solid dark:border-1 dark:shadow-[rgba(3,51,99,0.2)]">
+    <div
+      className={`ml-auto w-max dark:outline-[rgba(156,204,252,0.1)] dark:border-solid dark:border-1 dark:shadow-[rgba(3,51,99,0.2)] ${
+        classList && classList
+      }`}
+    >
       <input
         className="w-0 h-0 hidden dark_mode_input"
         type="checkbox"
-        id="darkmode-toggle"
+        id={id} // Use the unique ID
         onClick={toggleTheme}
       />
       <label
         className="w-[65px] h-[30px] relative block bg-[#ebebeb] rounded-full shadow-[inner-custom-light] cursor-pointer transition duration-300 dark_mode_label"
-        htmlFor="darkmode-toggle"
+        htmlFor={id} // Use the unique ID
       >
         <Sun className="absolute w-[20px] top-[5px] left-[5px] fill-white transition duration-300 sun" />
         <Moon className="absolute w-[20px] top-[5px] left-[40px] fill-[#7e7e7e] transition duration-300 moon" />
       </label>
     </div>
   );
+};
+
+ThemeButton.propTypes = {
+  classList: PropTypes.string,
+  onClick: PropTypes.func,
 };
 
 export default ThemeButton;
