9457d3dc6dc79345bde60254ccde2b50e65b4280
bmazurkiewicz01 <bartek.mazurkiewicz69@gmail.com>
2024-06-28 22:48:37 +0200

    Enhanced main layout

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,9 +3,11 @@ import MainLayout from "../layout/MainLayout";
 const About = () => {
   return (
     <MainLayout>
-      <div className="pt-32">
-        <h1>About Page</h1>
-        <p>Welcome to the About page.</p>
+      <div className="bg-gradient-to-b from-[#CEE5FD] to-white dark:from-[rgb(2,41,79)] dark:to-[rgba(9,14,16,0)] bg-no-repeat gradient-bg-size">
+        <div className="pt-32">
+          <h1>About Page</h1>
+          <p>Welcome to the About page.</p>
+        </div>
       </div>
     </MainLayout>
   );
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,31 +1,18 @@
 import Header from "../components/Header";
 import PropTypes from "prop-types";
+import "./MainLayout.css";
 
-const MainLayout = ({ children, parentClasses }) => {
+const MainLayout = ({ children }) => {
   return (
     <>
-      <div>
-        <Header />
-      </div>
-      <div
-        className={
-          parentClasses
-            ? parentClasses
-            : "bg-gradient-to-b from-[#CEE5FD] to-white dark:from-[rgb(2,41,79)] dark:to-[rgba(9,14,16,0)] bg-no-repeat"
-        }
-        style={{
-          backgroundSize: "100% 20%",
-        }}
-      >
-        {children}
-      </div>
+      <Header />
+      <main className="bg-[#fefeff] dark:bg-[#080f10]">{children}</main>
     </>
   );
 };
 
 MainLayout.propTypes = {
   children: PropTypes.node.isRequired,
-  parentClasses: PropTypes.string,
 };
 
 export default MainLayout;
