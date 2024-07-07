1a91430c50cda38def39cfeb4c9b619c1b60e673
bmazurkiewicz01 <bartek.mazurkiewicz69@gmail.com>
2024-07-07 22:58:31 +0200

    Added new social media icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -165,37 +165,7 @@ const Footer = ({ isDarkMode, className }) => {
               <li>
                 <Link
                   to="/"
-                  className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-700 dark:hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out"
-                  aria-label="Twitter"
-                >
-                  <svg
-                    className="w-8 h-8 fill-current"
-                    viewBox="0 0 32 32"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="m13.063 9 3.495 4.475L20.601 9h2.454l-5.359 5.931L24 23h-4.938l-3.866-4.893L10.771 23H8.316l5.735-6.342L8 9h5.063Zm-.74 1.347h-1.457l8.875 11.232h1.36l-8.778-11.232Z" />
-                  </svg>
-                </Link>
-              </li>
-              <li className="ml-4">
-                <Link
-                  to="/"
-                  className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-700 dark:hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out"
-                  aria-label="Github"
-                >
-                  <svg
-                    className="w-8 h-8 fill-current"
-                    viewBox="0 0 32 32"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M16 8.2c-4.4 0-8 3.6-8 8 0 3.5 2.3 6.5 5.5 7.6.4.1.5-.2.5-.4V22c-2.2.5-2.7-1-2.7-1-.4-.9-.9-1.2-.9-1.2-.7-.5.1-.5.1-.5.8.1 1.2.8 1.2.8.7 1.3 1.9.9 2.3.7.1-.5.3-.9.5-1.1-1.8-.2-3.6-.9-3.6-4 0-.9.3-1.6.8-2.1-.1-.2-.4-1 .1-2.1 0 0 .7-.2 2.2.8.6-.2 1.3-.3 2-.3s1.4.1 2 .3c1.5-1 2.2-.8 2.2-.8.4 1.1.2 1.9.1 2.1.5.6.8 1.3.8 2.1 0 3.1-1.9 3.7-3.7 3.9.3.4.6.9.6 1.6v2.2c0 .2.1.5.6.4 3.2-1.1 5.5-4.1 5.5-7.6-.1-4.4-3.7-8-8.1-8z" />
-                  </svg>
-                </Link>
-              </li>
-              <li className="ml-4">
-                <Link
-                  to="/"
-                  className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-700 dark:hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out"
+                  className="flex justify-center items-center border-solid border-[1px] border-[rgb(110,180,255)] text-[rgb(110,180,255)] dark:[rgb(110,180,255)] dark:bg-gray-800 hover:bg-[rgb(107,178,253)] hover:text-white dark:hover:text-gray-100 dark:hover:bg-[rgb(91,168,251)] dark:border-none rounded-full transition duration-150 ease-in-out"
                   aria-label="Facebook"
                 >
                   <svg
@@ -210,7 +180,7 @@ const Footer = ({ isDarkMode, className }) => {
               <li className="ml-4">
                 <Link
                   to="/"
-                  className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-700 dark:hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out"
+                  className="flex justify-center items-center border-solid border-[1px] border-[rgb(110,180,255)] text-[rgb(110,180,255)] dark:[rgb(110,180,255)] dark:bg-gray-800 hover:bg-[rgb(107,178,253)] hover:text-white dark:hover:text-gray-100 dark:hover:bg-[rgb(91,168,251)] dark:border-none rounded-full transition duration-150 ease-in-out"
                   aria-label="Instagram"
                 >
                   <svg
@@ -227,15 +197,19 @@ const Footer = ({ isDarkMode, className }) => {
               <li className="ml-4">
                 <Link
                   to="/"
-                  className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-700 dark:hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out"
-                  aria-label="Linkedin"
+                  className="flex justify-center items-center border-solid border-[1px] border-[rgb(110,180,255)] text-[rgb(110,180,255)] dark:[rgb(110,180,255)] dark:bg-gray-800 hover:bg-[rgb(107,178,253)] hover:text-white dark:hover:text-gray-100 dark:hover:bg-[rgb(91,168,251)] dark:border-none rounded-full transition duration-150 ease-in-out"
+                  aria-label="Tiktok"
                 >
                   <svg
-                    className="w-8 h-8 fill-current"
+                    className="w-8 h-8"
                     viewBox="0 0 32 32"
                     xmlns="http://www.w3.org/2000/svg"
                   >
-                    <path d="M23.3 8H8.7c-.4 0-.7.3-.7.7v14.7c0 .3.3.6.7.6h14.7c.4 0 .7-.3.7-.7V8.7c-.1-.4-.4-.7-.8-.7zM12.7 21.6h-2.3V14h2.4v7.6h-.1zM11.6 13c-.8 0-1.4-.7-1.4-1.4 0-.8.6-1.4 1.4-1.4.8 0 1.4.6 1.4 1.4-.1.7-.7 1.4-1.4 1.4zm10 8.6h-2.4v-3.7c0-.9 0-2-1.2-2s-1.4 1-1.4 2v3.8h-2.4V14h2.3v1c.3-.6 1.1-1.2 2.2-1.2 2.4 0 2.8 1.6 2.8 3.6v4.2h.1z" />
+                    <path
+                      d="M21.6,10.14a8.62,8.62,0,0,1-5.19-1.71A8.46,8.46,0,0,1,13.12,4,8.79,8.79,0,0,1,13,2.5H7.87V32.58a4.82,4.82,0,1,1-4.69-4.84,4.55,4.55,0,0,1,2.42.7V21.23a10.8,10.8,0,0,0-1.5-.11A10.93,10.93,0,1,0,14.25,32.14v-15a12,12,0,0,0,2.4,1.22,12.3,12.3,0,0,0,4.92.9Z"
+                      fill="currentColor"
+                      transform="translate(12, 5) scale(0.50)"
+                    />
                   </svg>
                 </Link>
               </li>
