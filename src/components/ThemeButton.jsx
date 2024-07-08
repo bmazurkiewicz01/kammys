a0e3405081a489f159decd82b97d156ccdcc20ee
bmazurkiewicz01 <bartek.mazurkiewicz69@gmail.com>
2024-07-08 13:18:09 +0200

    Fixed theme state in ThemeButton component

--- src/components/ThemeButton.jsx
+++ src/components/ThemeButton.jsx
@@ -1,29 +1,38 @@
-import { useEffect, useState, useId } from "react";
+import { useEffect, useState, useId, useRef } from "react";
 import { ReactComponent as Sun } from "../assets/Sun.svg";
 import { ReactComponent as Moon } from "../assets/Moon.svg";
 import PropTypes from "prop-types";
 import "./ThemeButton.css";
 
 const ThemeButton = ({ classList, onClick }) => {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme")
+      ? localStorage.getItem("theme")
+      : window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light"
+  );
   const id = useId();
+  const selectedThemeRef = useRef(theme);
 
   useEffect(() => {
     let savedTheme = localStorage.getItem("theme");
     if (!savedTheme) {
       const userPrefersDark = window.matchMedia(
-        "(prefers-color-scheme: dark)",
+        "(prefers-color-scheme: dark)"
       ).matches;
       setTheme(userPrefersDark ? "dark" : "light");
       localStorage.setItem("theme", userPrefersDark ? "dark" : "light");
+      selectedThemeRef.current = userPrefersDark ? "dark" : "light";
     } else {
       setTheme(savedTheme);
+      selectedThemeRef.current = savedTheme;
     }
     if (savedTheme === "dark") {
       document.documentElement.classList.add("dark");
       document.querySelector("body").classList.add("dark");
     }
-  }, []);
+  }, [selectedThemeRef]);
 
   const toggleTheme = () => {
     if (theme === "light") {
@@ -40,9 +49,6 @@ const ThemeButton = ({ classList, onClick }) => {
     onClick && onClick(theme);
   };
 
-  const selectedTheme =
-    localStorage.getItem("theme") === "dark" ? "dark" : "light";
-
   return (
     <div
       className={`ml-auto w-max dark:outline-[rgba(156,204,252,0.1)] dark:border-solid dark:border-1 dark:shadow-[rgba(3,51,99,0.2)] ${
@@ -54,7 +60,7 @@ const ThemeButton = ({ classList, onClick }) => {
         type="checkbox"
         id={id}
         onClick={toggleTheme}
-        defaultChecked={selectedTheme === "dark"}
+        defaultChecked={selectedThemeRef.current === "dark"}
       />
       <label
         className="w-[65px] h-[30px] relative block bg-[#ebebeb] rounded-full shadow-[inner-custom-light] cursor-pointer transition duration-300 dark_mode_label"
